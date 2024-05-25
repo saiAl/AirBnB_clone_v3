05f728e9ea66800dd9ca64dc067299eae5852754
sai <ojiip95@gmail.com>
2024-05-25 18:14:16 +0200

    fix typos in task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,18 +2,13 @@
 """create app instance of Flask\
         register the blueprint app_views to your Flask instance app"""
 
-
 from os import getenv
 from flask import Flask
-# from app_views import api.v1.views
 from api.v1.views import app_views
-# from storage import models
 from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-host = getenv("HBNB_API_HOST", "0.0.0.0")
-port = getenv("HBNB_API_PORT", "5000")
 
 
 @app.teardown_appcontext
@@ -23,4 +18,7 @@ def teardown(exception):
 
 
 if __name__ == "__main__":
-    app.run(host, port, threaded=True, debug=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+
+    app.run(host, port, threaded=True)
