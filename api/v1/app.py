9f4332b4e390d6a88045cecf8329c89cacd3a2d8
sai <ojiip95@gmail.com>
2024-05-25 19:27:41 +0200

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,17 @@
 """ create Flask instance"""
 
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def pageNotFound(exception):
+    return jsonify({"error": "Not found"})
 
 
 @app.teardown_appcontext
@@ -20,4 +25,4 @@ if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", 5000)
 
-    app.run(host, port, threaded=True)
+    app.run(host, port, debug=True, threaded=True)
